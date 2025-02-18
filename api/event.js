818ceef1f575822595f4cb2038369a71ec4ed7a5
Ananto, Azizul <azizul.ananto@kpn.com>
2025-02-18 22:03:11 +0100

    Fix event api

--- api/event.js
+++ api/event.js
@@ -4,18 +4,18 @@ import cors from "./cors.js";
 export default async function handler(req, res) {
   cors(req, res, async () => {
     if (req.method === "POST") {
-      const { room_id, events } = req.body;
+      const { room_id, messages } = req.body;
 
       const members = await getRoomMembers(room_id);
       if (!members.length) {
         return res.status(404).json({ error: "Room not found" });
       }
 
-      if (!Array.isArray(events)) {
+      if (!Array.isArray(messages)) {
         return res.status(400).json({ error: "Events should be an array" });
       }
 
-      await addRoomEvents(room_id, events);
+      await addRoomEvents(room_id, messages);
 
       return res.json({ success: true });
     } else {
